0a1dc71fdf68554f7436b1274fb5a724f958aa2a
Vahid Hasani <vahidhasani09@gmail.com>
2021-04-29 04:53:37 +0430

    add skeleton ui

--- src/Components/Content/index.tsx
+++ src/Components/Content/index.tsx
@@ -2,43 +2,16 @@ import { Container, CssBaseline, Grid, Typography } from "@material-ui/core";
 import { Fragment, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco, darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { useHistory } from "react-router-dom";
 import Camalize from "../../Utils/Camalize";
 import { useContent } from "../../Hooks/useContent";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 function Content() {
 	const history = useHistory();
-	const getContent = useContent(Camalize(history.location.pathname));
+	const content = useContent(Camalize(history.location.pathname));
 
-	// useEffect(() => {
-	// 	console.log(getContent);
-	// }, [getContent]);
-
-	const code = `package main
-
-    import "fmt"
-    
-    func zeroval(ival int) {
-        ival = 0
-    }
-    
-    func zeroptr(iptr *int) {
-        *iptr = 0
-    }
-    
-    func main() {
-        i := 1
-        fmt.Println("initial:", i)
-    
-        zeroval(i)
-        fmt.Println("zeroval:", i)
-    
-        zeroptr(&i)
-        fmt.Println("zeroptr:", i)
-    
-        fmt.Println("pointer:", &i)
-    }`;
 	return (
 		<Fragment>
 			<CssBaseline />
@@ -51,18 +24,33 @@ function Content() {
 				>
 					<Grid item xs={12} sm={6} lg={5}>
 						<Typography variant="h4">
-							{getContent.title.length ? getContent.title : "Not Found"}
+							{content.title.length ? (
+								content.title
+							) : (
+								<Skeleton variant="text" animation="wave" />
+							)}
 						</Typography>
 
 						<Typography variant="body1">
-							<ReactMarkdown>
-								{getContent.content.length ? getContent.content : "Not Found"}
-							</ReactMarkdown>
+							{content.content.length ? (
+								<ReactMarkdown>{content.content}</ReactMarkdown>
+							) : (
+								<>
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+									<Skeleton variant="text" animation="wave" />
+								</>
+							)}
 						</Typography>
 					</Grid>
 					<Grid item xs={12} sm={6} lg={5}>
 						<SyntaxHighlighter language="go" style={darcula}>
-							{getContent.code ? getContent.code : "There is no code"}
+							{content.code ? content.code : "There is no code"}
 						</SyntaxHighlighter>
 					</Grid>
 				</Grid>
